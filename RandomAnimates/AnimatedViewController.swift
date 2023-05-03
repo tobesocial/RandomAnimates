96f3fbbf5f550f70ad8903e04c9fd09de28c8c7c
Dmitry Fedorov <divinedavidbrown@yandex.ru>
2023-05-03 17:57:09 +0300

    final changed

--- RandomAnimates/AnimatedViewController.swift
+++ RandomAnimates/AnimatedViewController.swift
@@ -10,23 +10,57 @@ import SpringAnimation
 
 final class AnimatedViewController: UIViewController {
     
+    var animation = Animation.getAnimations()
+    
     @IBOutlet weak var springAnimationView: SpringView!
+    
+    @IBOutlet weak var presentLabel: UILabel!
+    @IBOutlet weak var curveLabel: UILabel!
+    @IBOutlet weak var forceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var delayLabel: UILabel!
+    
+    @IBOutlet weak var animateButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-
-    @IBAction func runSpringAnimation(_ sender: SpringButton) {
-        springAnimationView.animation = "fall"
-        springAnimationView.curve = "easeInOut"
-        springAnimationView.animate()
+        springAnimationView.layer.cornerRadius = 15
+        animateButton.layer.cornerRadius = 15
         
-        sender.animation = "pop"
-        sender.duration = 3
-        sender.animate()
+        setValueLabels()
     }
     
+    @IBAction func animateButtonPressed() {
+        makeAnimation()
+        setValueLabels()
+        updateButtonTitle()
+    }
+    
+    private func makeAnimation() {
+        springAnimationView.animation = animation.title
+        springAnimationView.curve = animation.curve
+        springAnimationView.force = animation.force
+        springAnimationView.duration = animation.duration
+        springAnimationView.damping = animation.damping
+        springAnimationView.delay = animation.delay
+        springAnimationView.animate()
+    }
+    
+    private func setValueLabels() {
+        presentLabel.text = animation.title
+        curveLabel.text = animation.curve
+        forceLabel.text = String(format: "%.2f", animation.force)
+        durationLabel.text = String(format: "%.2f", animation.duration)
+        delayLabel.text = String(format: "%.2f", animation.delay)
+    }
+    
+    private func updateButtonTitle() {
+        let nextAnimation = Animation.getAnimations()
+        let buttonTitle = "Run \(nextAnimation.title)"
+        animateButton.setTitle(buttonTitle, for: .normal)
+        animation = nextAnimation
+    }
+
 }
 
--- RandomAnimates/Animation.swift
+++ RandomAnimates/Animation.swift
@@ -17,34 +17,22 @@ struct Animation {
     let duration: Double
     let damping: Double
     
-    static func getAnimations() -> [Animation] {
-        var animations: [Animation] = []
+    static func getAnimations() -> Animation {
+        let title = DataStore.shared.animations.randomElement() ?? "pop"
+        let curve = DataStore.shared.curves.randomElement() ?? "easeIn"
         
-        let title = DataStore.shared.title.shuffled()
-        let curve = DataStore.shared.curve.shuffled()
-        let force = Double.random(in: 1.0...5.0)
-        let delay = Double.random(in: 1.0...2.0)
-        let duration = Double.random(in: 0.5...5.0)
-        let damping = Double.random(in: 0.0...5.0)
+        let force = Double.random(in: 0.5...1.5)
+        let duration = Double.random(in: 1.0...2.0)
+        let delay = Double.random(in: 0.0...0.5)
+        let damping = Double.random(in: 0.5...1.0)
         
-        let iterationCount = min(
-            title.count,
-            curve.count
+        return Animation(
+            title: title,
+            curve: curve,
+            force: force,
+            delay: delay,
+            duration: duration,
+            damping: damping
         )
-        
-        for index in 0..<iterationCount {
-            let animation = Animation(
-                title: title[index],
-                curve: curve[index],
-                force: force,
-                delay: delay,
-                duration: duration,
-                damping: damping
-            )
-            
-            animations.append(animation)
-        }
-        
-        return animations
     }
 }
